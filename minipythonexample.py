9223266b41a0443e40546f399c02ce3cce8254a4
JimTsiob <53694372+JimTsiob@users.noreply.github.com>
2021-10-28 19:53:53 +0300

    Add files via upload

---
+++ minipythonexample.py
@@ -0,0 +1,12 @@
+# minipython example
+
+def add(x,y):
+    return x+y
+
+word = "hello friends!" # anathesh symboloseiras se metavlhth
+intNum = 10 # anathesh akeraiou arithmou se metavlhth
+floatNum = 9.5 # anathesh dekadikou arithmou se metavlhth
+testList = ["apple","banana","orange"] # anathesh listas se metavlhth
+
+
+print(add(5,10))
