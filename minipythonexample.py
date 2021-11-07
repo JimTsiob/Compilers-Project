5186f229bf66d976b370c76bf329d88849b0e9f2
Ana <anna.lesi2000@gmail.com>
2021-11-07 12:19:20 +0200

    added string with single quotes

--- minipythonexample.py
+++ minipythonexample.py
@@ -4,13 +4,13 @@ from math import sqrt
 def add(x,y):
     return x+y
 # anathesh symboloseiras se metavlhth
-word = "hello friends!" 
-# anathesh akeraiou arithmou se metavlhth
+word = "hello friends!" # anathesh akeraiou arithmou se metavlhth
 intNum = 10 
 # anathesh dekadikou arithmou se metavlhth
 floatNum = 9.5 
 # anathesh listas se metavlhth
-testList = ["apple","banana","orange"] 
-
+testList = ["apple","banana","orange"]
+stringLit = 'yo'
+a0 = 3**2 + 7*8%2
 
 print add(5,10)
