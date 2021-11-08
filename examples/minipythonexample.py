aea046375383fbb89e6a76d14687c70be9c410b6
Ana <anna.lesi2000@gmail.com>
2021-11-08 14:28:11 +0200

    chore: store examples in examples folder

---
+++ examples/minipythonexample.py
@@ -0,0 +1,16 @@
+# minipython example
+from math import sqrt
+
+def add(x,y):
+    return x+y
+# anathesh symboloseiras se metavlhth
+word = "hello friends!" # anathesh akeraiou arithmou se metavlhth
+intNum = 10 
+# anathesh dekadikou arithmou se metavlhth
+floatNum = 9.5 
+# anathesh listas se metavlhth
+testList = ["apple","banana","orange"]
+stringLit = 'yo'
+a0 = 3**2 + 7*8%2
+
+print add(5,10)
